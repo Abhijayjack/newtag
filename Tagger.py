1f4acbba4647e84d8b0d3867f1e4e455c77943f1
Abhijayjack <102459537+Abhijayjack@users.noreply.github.com>
2022-04-04 16:33:54 +0300

    Update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -36,7 +36,7 @@ async def start(event):
                    )
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**🌀 OnlineTaggerBot Komutları**\n\n**/tag <sebeb> - 5-li Etiket Atar**\n\n**/etag <sebeb> - Emoji ile etiketler**\n\n**/tektag sebeb - Üyeleri Tek Tek Etiketler**\n\n**/admins sebeb - Yöneticileri Tek Tek Tag Eder**\n\n**/start - botu başlatır**"
+  helptext = "**🌀 OnlineTaggerBot Komutları**\n\n**/tag <sebeb> - 5-li Etiket Atar**\n\n**/etag <sebeb> - Emoji ile etiketler**\n\n**/tektag sebeb - Üyeleri Tek Tek Etiketler**\n\n**/atag sebeb - Sadece yöneticileri etiket Eder**\n\n**/start - botu başlatır**"
   await event.reply(helptext,
                     buttons=(
                       [Button.url('Beni Gruba Ekle➕', 'https://t.me/userstagbot?startgroup=a')],
@@ -265,23 +265,66 @@ async def cancel(event):
 	
 
 
-@client.on(events.NewMessage(pattern="^/admins ?(.*)"))
-async def mentionall(tagadmin):
 
-	if tagadmin.pattern_match.group(1):
-		seasons = tagadmin.pattern_match.group(1)
-	else:
-		seasons = ""
+@client.on(events.NewMessage(pattern="^/atag ?(.*)"))
+async def mentionalladmin(event):
 
-	chat = await tagadmin.get_input_chat()
-	a_=0
-	await tagadmin.delete()
-	async for i in client.iter_participants(chat, filter=cp):
-		if a_ == 500:
-			break
-		a_+=5
-		await tagadmin.client.send_message(tagadmin.chat_id, "**[{}](tg://user?id={}) {}**".format(i.first_name, i.id, seasons))
-		sleep(0.5)
+  if event.is_private:
+    return await event.respond("__Bu komut gruplarda ve kanallarda kullanılabilir.!__")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("__Yalnızca yöneticiler hepsinden bahsedebilir!__")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__Eski mesajlar için üyelerden bahsedemem! (gruba eklemeden önce gönderilen mesajlar)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Bana bir argüman ver!__")
+  else:
+    return await event.respond("__Bir mesajı yanıtlayın veya başkalarından bahsetmem için bana bir metin verin!__")
+  
+  if mode == "text_on_cmd":
+    anlik_calisan.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id,filter=ChannelParticipantsAdmins):
+      usrnum += 1
+      usrtxt += f"👥-[{usr.first_name}](tg://user?id={usr.id})\n "
+      if event.chat_id not in anlik_calisan:
+        await event.respond("Etikeletme İşlemi Bitti 👥 İyi günler dileriz 🤗")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{msg}\n\n{usrtxt}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+    anlik_calisan.remove(event.chat_id)
+  
+  if mode == "text_on_reply":
+    anlik_calisan.append(event.chat_id)
+ 
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id,filter=ChannelParticipantsAdmins):
+      usrnum += 1
+      usrtxt += f"👥-[{usr.first_name}](tg://user?id={usr.id})\n "
+      if event.chat_id not in anlik_calisan:
+        await event.respond("İşlem Başarılı Bir Şekilde Durduruldu ❌")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+    anlik_calisan.remove(event.chat_id)
 
 
 print(">> Bot çalıyor merak etme 🚀 @sohbetonlinesahip bilgi alabilirsin <<")
